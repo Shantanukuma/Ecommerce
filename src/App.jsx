25f92a99fd56de46a8d436ec3dfa6150ff88c523
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-05-14 11:40:17 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
+import Home from './Pages/home/Home'
+import Cart from './Pages/cart/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='bg-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/cart' element = {<Cart/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
