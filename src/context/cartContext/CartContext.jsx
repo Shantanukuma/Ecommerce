686cb8278a731ff439637cb1ef029069ce8a948f
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-05-15 12:30:27 +0530

    faker js

--- src/context/cartContext/CartContext.jsx
+++ src/context/cartContext/CartContext.jsx
@@ -6,8 +6,21 @@ const CartContext = createContext();
 const CartContextProvider = ({ children }) => {
 
     const products = [...Array(20)].map( (product) => ({
-        id:faker.string.uuid()
-    }))
+        id:faker.string.uuid(),
+        productName: faker.commerce.productName(),
+        productDescription: faker.commerce.productDescription(),
+        price: faker.commerce.price({min: 100, max: 5000}),
+        image: faker.image.urlPicsumPhotos({
+            width: 300,
+            height: 300
+        }),
+        instock: faker.helpers.arrayElement([0, 5, 10, 15, 30]),
+        fastDeliver: faker.datatype.boolean(),
+        new: faker.datatype.boolean(),
+        ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+        
+
+    }));
     const state = {
         products
     }
